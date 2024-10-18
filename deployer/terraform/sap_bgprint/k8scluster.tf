0dcab40725579937a744193e2053bf772afcef7e
devanshjain <devanshjain@microsoft.com>
2024-10-18 00:02:20 +0000

    Refactor k8scluster.tf to reduce default node count to 2

--- deployer/terraform/sap_bgprint/k8scluster.tf
+++ deployer/terraform/sap_bgprint/k8scluster.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
     default_node_pool {
         name                = "default"
-        node_count          = 5
+        node_count          = 2
         vm_size             = "Standard_DS3_v2"
         vnet_subnet_id      = azurerm_subnet.subnet.id
     }
